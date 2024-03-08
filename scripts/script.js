1448be39cdd01809dfd63de1ccedffe2f9a31987
Kush Patel <111901629+kushpatelj86@users.noreply.github.com>
2024-03-08 14:01:33 -0800

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,33 @@
 
+function calcBMI(){
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+
+    let bmi = Math.round(((weight / (height * height) * 703 )) * 10) / 10;
+
+    document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
+    document.getElementById("BMI").innerHTML = bmi;
+
+
+    if(bmi < 18.5)
+    {
+        document.getElementById("Health-Status").innerHTML = "You are Underweight";
+    }
+    else if (bmi >= 18.5 && bmi <= 24.9)
+    {
+        document.getElementById("Health-Status").innerHTML = "You are Normal Weight";
+    }
+    else if (bmi >= 25 && bmi <= 29.9)
+    {
+        document.getElementById("Health-Status").innerHTML = "You are Overweight";
+    }
+    else if (bmi >= 30)
+    {
+        document.getElementById("Health-Status").innerHTML = "You have Obesity";
+    }
+    else
+    {
+        
+    }
+}
