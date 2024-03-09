66366245555f37fa9cabf5adc129dba1bd396259
moksh <mokshp33@gmail.com>
2024-03-08 22:43:33 -0800

    Changed messages for user

--- scripts/script.js
+++ scripts/script.js
@@ -12,19 +12,19 @@ function calcBMI(){
 
     if(bmi < 18.5)
     {
-        document.getElementById("Health-Status").innerHTML = "You are Underweight";
+        document.getElementById("Health-Status").innerHTML = "You are underweight. Consider nutritional consultation with us to assess your diet";
     }
     else if (bmi >= 18.5 && bmi <= 24.9)
     {
-        document.getElementById("Health-Status").innerHTML = "You are Normal Weight";
+        document.getElementById("Health-Status").innerHTML = "You are a healthy weight. Mantain your current diet and excercise routine."; 
     }
     else if (bmi >= 25 && bmi <= 29.9)
     {
-        document.getElementById("Health-Status").innerHTML = "You are Overweight";
+        document.getElementById("Health-Status").innerHTML = "You are overweight. Consider moderate excercise and consulting us to review your diet.";
     }
     else if (bmi >= 30)
     {
-        document.getElementById("Health-Status").innerHTML = "You have Obesity";
+        document.getElementById("Health-Status").innerHTML = "You have Obesity. It's important to seek consulting from us to develop a personalized diet and excercise plan.";
     }
     else
     {
