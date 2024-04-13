cf64cbcbe12f9d79e36f67d3610a882bde6ca45a
Kush Patel <111901629+kushpatelj86@users.noreply.github.com>
2024-04-12 22:08:16 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -78,121 +78,6 @@ function displayWeeklyWorkouts() {
       checkboxes.forEach((checkbox) => {
         selectedWorkouts.push(checkbox.value);
       });
-      document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(", ");
-    });
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
-
-  function generatePlan(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    document.getElementById('plan').innerHTML = `Loading...`;
-
-    fetch('/api/users')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('plan').innerHTML = JSON.stringify(data, null, 2);
-    })
-    .catch(err => {
-        console.error('Error:', err);
-        document.getElementById('plan').innerHTML = `Error fetching data`;
-    });
-}
-
-
-
-
-
-
-
-
-
-"""
-
-
-function calcBMI(){
-    let weight = document.getElementById("weight").value;
-    let height = document.getElementById("height").value;
-
-
-    let bmi = Math.round(((weight / (height * height) * 703 )) * 10) / 10;
-
-    document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
-    
-    
-    if(isNaN(weight) || isNaN(height) || weight == null || height == null || weight == "" || height == "")
-    {
-        
-        
-        document.getElementById("BMI-Heading").innerHTML = "Please Enter Both Height and Weight ";
-        document.getElementById("BMI").innerHTML = "";
-        document.getElementById("Health-Status").innerHTML = "Unable to calculate to BMI";
-
-
-        
-    }
-    else
-    {
-        document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
-        document.getElementById("BMI").innerHTML = bmi;
-
-            if(bmi < 18.5)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are underweight. Consider nutritional consultation with us to assess your diet";
-            }
-            else if (bmi >= 18.5 && bmi <= 24.9)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are a healthy weight. Mantain your current diet and excercise routine."; 
-            }
-            else if (bmi >= 25 && bmi <= 29.9)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are overweight. Consider moderate excercise and consulting us to review your diet.";
-            }
-            else if (bmi >= 30)
-            {
-                document.getElementById("Health-Status").innerHTML = "You have Obesity. It's important to seek consulting from us to develop a personalized diet and excercise plan.";
-            }
-            else
-            {
-                
-            }
-    }
-
-    
-}
-
-function reload()
-{
-    window.location.reload();
-}
-
-function displayWeeklyWorkouts() {
-    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    days.forEach(day => {
-      var selectedWorkouts = [];
-      var checkboxes = document.querySelectorAll(`input[name='${day}']:checked`);
-      checkboxes.forEach((checkbox) => {
-        selectedWorkouts.push(checkbox.value);
-      });
-      document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(", ");
-    });
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-  function displayWeeklyWorkouts() {
-    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    days.forEach(day => {
-      var selectedWorkouts = [];
-      var checkboxes = document.querySelectorAll(`input[name='${day}']:checked`);
-      checkboxes.forEach((checkbox) => {
-        selectedWorkouts.push(checkbox.value);
-      });
 
       
       document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(', ');
@@ -240,427 +125,94 @@ function displayWeeklyWorkouts() {
 
 
 
-function calcBMI(){
-    let weight = document.getElementById("weight").value;
-    let height = document.getElementById("height").value;
-
-
-    let bmi = Math.round(((weight / (height * height) * 703 )) * 10) / 10;
-
-    document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
-    
-    
-    if(isNaN(weight) || isNaN(height) || weight == null || height == null || weight == "" || height == "")
-    {
-        
-        
-        document.getElementById("BMI-Heading").innerHTML = "Please Enter Both Height and Weight ";
-        document.getElementById("BMI").innerHTML = "";
-        document.getElementById("Health-Status").innerHTML = "Unable to calculate to BMI";
-
-
-        
-    }
-    else
-    {
-        document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
-        document.getElementById("BMI").innerHTML = bmi;
-
-            if(bmi < 18.5)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are underweight. Consider nutritional consultation with us to assess your diet";
-            }
-            else if (bmi >= 18.5 && bmi <= 24.9)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are a healthy weight. Mantain your current diet and excercise routine."; 
-            }
-            else if (bmi >= 25 && bmi <= 29.9)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are overweight. Consider moderate excercise and consulting us to review your diet.";
-            }
-            else if (bmi >= 30)
-            {
-                document.getElementById("Health-Status").innerHTML = "You have Obesity. It's important to seek consulting from us to develop a personalized diet and excercise plan.";
-            }
-            else
-            {
-                
-            }
-    }
-
-    
-}
-
-function reload()
-{
-    window.location.reload();
-}
-
-function displayWeeklyWorkouts() {
-    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    days.forEach(day => {
-      var selectedWorkouts = [];
-      var checkboxes = document.querySelectorAll(`input[name='${day}']:checked`);
-      checkboxes.forEach((checkbox) => {
-        selectedWorkouts.push(checkbox.value);
-      });
-      document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(", ");
-    });
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-  function displayWeeklyWorkouts() {
-    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    days.forEach(day => {
-      var selectedWorkouts = [];
-      var checkboxes = document.querySelectorAll(`input[name='${day}']:checked`);
-      checkboxes.forEach((checkbox) => {
-        selectedWorkouts.push(checkbox.value);
-      });
-
-      
-      document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(', ');
-      
-      fetch('http://127.0.0.1:3456/workout/plan')
-    .then(response => response.json() )
-    .then(data => {
-        a = JSON.stringify(data, null, 2);
-        document.getElementById(`display${capitalize(day)}`).innerHTML += `(${a})`
-
-    })
-    .catch(err => {
-      
-        document.getElementById(`display${capitalize(day)}`).innerHTML += `(${err.message})`
-
-    });
-
-
-
-
-
-    });
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
-
-  function generatePlan(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    document.getElementById('plan').innerHTML = `Loading...`;
-
-    fetch('http://127.0.0.1:3456/users')
-    .then(response => response.json() )
-    .then(data => {
-        document.getElementById('plan').innerHTML = JSON.stringify(data, null, 2);
-    })
-    .catch(err => {
-        document.getElementById('plan').innerHTML = `Error fetching data: ${err.message}`;
-    });
-}
-
-
-
-
-function loginUser(event) {
+function create_user(event) {
   event.preventDefault();
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  // Perform login logic, e.g., send data to server for authentication
-}
+  var name = document.getElementById('name').value;
+  var weight = document.getElementById('weight').value;
+  var height = document.getElementById('height').value;
+  var age = document.getElementById('age').value;
+  var allergies = document.getElementById('allergies').value;
 
-function signupUser(event) {
-  event.preventDefault();
-  let newUsername = document.getElementById("new-username").value;
-  let newPassword = document.getElementById("new-password").value;
-  // Perform sign-up logic, e.g., send data to server for registration
-}
-
-
-
-
-document.getElementById("login-form").addEventListener("submit", loginUser);
-document.getElementById("signup-form").addEventListener("submit", signupUser)
+  var formData = {
+      name: name.toString(),
+      weight: weight.toString(),
+      height: height.toString(),
+      age: age.toString(),
+      allergies: allergies.toString()
+  };
 
 
+  fetch('/create_user', {
+    method: 'POST',  // Ensure this matches the expected method in your Flask route
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Handle response data
+    alert('User created successfully, '+ data);
+  })
+  .catch(error => {
+    alert('Error creating user: ' + error)
+  });
 
-function calcBMI(){
-    let weight = document.getElementById("weight").value;
-    let height = document.getElementById("height").value;
 
 
-    let bmi = Math.round(((weight / (height * height) * 703 )) * 10) / 10;
 
-    document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
-    
-    
-    if(isNaN(weight) || isNaN(height) || weight == null || height == null || weight == "" || height == "")
-    {
+  /*$.ajax({
+      type: 'POST',
+      url: '/create_user', // Assuming this is the correct endpoint on your backend
+      contentType: 'application/json', // Make sure this line is present and correctly set
+      data: JSON.stringify(formData),
+      success: function(response) {
+          // Update message container with success message from server
+          $('#message').text(response.message);
         
-        
-        document.getElementById("BMI-Heading").innerHTML = "Please Enter Both Height and Weight ";
-        document.getElementById("BMI").innerHTML = "";
-        document.getElementById("Health-Status").innerHTML = "Unable to calculate to BMI";
-
-
-        
-    }
-    else
-    {
-        document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
-        document.getElementById("BMI").innerHTML = bmi;
-
-            if(bmi < 18.5)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are underweight. Consider nutritional consultation with us to assess your diet";
-            }
-            else if (bmi >= 18.5 && bmi <= 24.9)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are a healthy weight. Mantain your current diet and excercise routine."; 
-            }
-            else if (bmi >= 25 && bmi <= 29.9)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are overweight. Consider moderate excercise and consulting us to review your diet.";
-            }
-            else if (bmi >= 30)
-            {
-                document.getElementById("Health-Status").innerHTML = "You have Obesity. It's important to seek consulting from us to develop a personalized diet and excercise plan.";
-            }
-            else
-            {
-                
-            }
-    }
-
-    
+      },
+      error: function(xhr, status, error) {
+          // Update message container with error message
+          $('#message').text('Error creating user: ' + error);
+      }
+  });*/
 }
 
-function reload()
-{
-    window.location.reload();
-}
-
-function displayWeeklyWorkouts() {
-    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    days.forEach(day => {
-      var selectedWorkouts = [];
-      var checkboxes = document.querySelectorAll(`input[name='${day}']:checked`);
-      checkboxes.forEach((checkbox) => {
-        selectedWorkouts.push(checkbox.value);
-      });
-      document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(", ");
-    });
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-  function displayWeeklyWorkouts() {
-    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    days.forEach(day => {
-      var selectedWorkouts = [];
-      var checkboxes = document.querySelectorAll(`input[name='${day}']:checked`);
-      checkboxes.forEach((checkbox) => {
-        selectedWorkouts.push(checkbox.value);
-      });
-
-      
-      document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(', ');
-      
-      fetch('http://127.0.0.1:3456/workout/plan')
-    .then(response => response.json() )
-    .then(data => {
-        a = JSON.stringify(data, null, 2);
-        document.getElementById(`display${capitalize(day)}`).innerHTML += `(${a})`
-
-    })
-    .catch(err => {
-      
-        document.getElementById(`display${capitalize(day)}`).innerHTML += `(${err.message})`
-
-    });
+/*fetch('/create_user', {
+  method: 'POST',  // Ensure this matches the expected method in your Flask route
+  headers: {
+      'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(formData),
+})
+.then(response => response.json())
+.then(data => {
+  // Handle response data
+  alert('User created successfully, '+ data);
+})
+.catch(error => {
+  alert('Error creating user: ' + error)
+});*/
 
 
 
 
 
-    });
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
-
-  function generatePlan(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    document.getElementById('plan').innerHTML = `Loading...`;
-
-    fetch('http://127.0.0.1:3456/users')
-    .then(response => response.json() )
-    .then(data => {
-        document.getElementById('plan').innerHTML = JSON.stringify(data, null, 2);
-    })
-    .catch(err => {
-        document.getElementById('plan').innerHTML = `Error fetching data: ${err.message}`;
-    });
-}
-
-
-
-
-function loginUser(event) {
-  event.preventDefault();
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  // Perform login logic, e.g., send data to server for authentication
-}
-
-function signupUser(event) {
-  event.preventDefault();
-  let newUsername = document.getElementById("new-username").value;
-  let newPassword = document.getElementById("new-password").value;
-  // Perform sign-up logic, e.g., send data to server for registration
-}
-
-function navigate()
-{
-  if(!document.getElementById("login-form").hasEventListener("submit", loginUser) || 
-  !document.getElementById("login-form").hasEventListener("submit", loginUser))
-  {
-    
-  }
-}
-
-
-
-function calcBMI(){
-    let weight = document.getElementById("weight").value;
-    let height = document.getElementById("height").value;
-
-
-    let bmi = Math.round(((weight / (height * height) * 703 )) * 10) / 10;
-
-    document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
-    
-    
-    if(isNaN(weight) || isNaN(height) || weight == null || height == null || weight == "" || height == "")
-    {
-        
-        
-        document.getElementById("BMI-Heading").innerHTML = "Please Enter Both Height and Weight ";
-        document.getElementById("BMI").innerHTML = "";
-        document.getElementById("Health-Status").innerHTML = "Unable to calculate to BMI";
-
-
-        
-    }
-    else
-    {
-        document.getElementById("BMI-Heading").innerHTML = "Your BMI is: ";
-        document.getElementById("BMI").innerHTML = bmi;
-
-            if(bmi < 18.5)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are underweight. Consider nutritional consultation with us to assess your diet";
-            }
-            else if (bmi >= 18.5 && bmi <= 24.9)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are a healthy weight. Mantain your current diet and excercise routine."; 
-            }
-            else if (bmi >= 25 && bmi <= 29.9)
-            {
-                document.getElementById("Health-Status").innerHTML = "You are overweight. Consider moderate excercise and consulting us to review your diet.";
-            }
-            else if (bmi >= 30)
-            {
-                document.getElementById("Health-Status").innerHTML = "You have Obesity. It's important to seek consulting from us to develop a personalized diet and excercise plan.";
-            }
-            else
-            {
-                
-            }
-    }
-
-    
-}
-
-function reload()
-{
-    window.location.reload();
-}
-
-function displayWeeklyWorkouts() {
-    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    days.forEach(day => {
-      var selectedWorkouts = [];
-      var checkboxes = document.querySelectorAll(`input[name='${day}']:checked`);
-      checkboxes.forEach((checkbox) => {
-        selectedWorkouts.push(checkbox.value);
-      });
-      document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(", ");
-    });
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-  function displayWeeklyWorkouts() {
-    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    days.forEach(day => {
-      var selectedWorkouts = [];
-      var checkboxes = document.querySelectorAll(`input[name='${day}']:checked`);
-      checkboxes.forEach((checkbox) => {
-        selectedWorkouts.push(checkbox.value);
-      });
-
-      
-      document.getElementById(`display${capitalize(day)}`).innerHTML = `${capitalize(day)}'s Selected Workouts: ` + selectedWorkouts.join(', ');
-      
-      fetch('http://127.0.0.1:3456/workout/plan')
-    .then(response => response.json() )
-    .then(data => {
-        a = JSON.stringify(data, null, 2);
-        document.getElementById(`display${capitalize(day)}`).innerHTML += `(${a})`
-
-    })
-    .catch(err => {
-      
-        document.getElementById(`display${capitalize(day)}`).innerHTML += `(${err.message})`
-
-    });
 
 
 
 
 
-    });
-  }
-  
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
 
 
-  function generatePlan(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
 
-    document.getElementById('plan').innerHTML = `Loading...`;
 
-    fetch('http://127.0.0.1:3456/users')
-    .then(response => response.json() )
-    .then(data => {
-        document.getElementById('plan').innerHTML = JSON.stringify(data, null, 2);
-    })
-    .catch(err => {
-        document.getElementById('plan').innerHTML = `Error fetching data: ${err.message}`;
-    });
-}
+
+
+
+
+
+
 
 
 
@@ -685,14 +237,4 @@ function navigate(event)
   
  
 }
-
-
-
-"""
-
-
-
-
-
-
 
